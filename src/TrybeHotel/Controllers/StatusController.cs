38b96a0f7ee8bd2f93f45501bddc2314517880e0
delso-ferreira <delsofelipe@gmail.com>
2024-05-21 15:36:10 -0300

    fix: controllers

--- src/TrybeHotel/Controllers/StatusController.cs
+++ src/TrybeHotel/Controllers/StatusController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace TrybeHotel.Controllers
 {
     [ApiController]
-    [Route("/")]
+    [Route("/status")]
     public class StatusController : Controller
     {
     
