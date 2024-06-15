1455cffc9552456f8966379748fa4c526d4f49cd
delso-ferreira <delsofelipe@gmail.com>
2024-06-15 13:52:58 -0300

    feat: test get from body

--- src/TrybeHotel/Controllers/UserController.cs
+++ src/TrybeHotel/Controllers/UserController.cs
@@ -36,7 +36,7 @@ namespace TrybeHotel.Controllers
 
         [HttpGet]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public IActionResult GetUserByEmail([FromQuery] string email)
+        public IActionResult GetUserByEmail([FromBody] string email)
         {
             try
             {
